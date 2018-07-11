72b5f79df090a2559fc756ff26326d149556613e
john pope <johnpope@johns-MacBook-Pro.local>
2018-07-11 17:37:58 -0400

    styling things

--- src/js/main.js
+++ src/js/main.js
@@ -1,24 +1,4 @@
 $(() => {
-  // const incrementNumber = n => Math.floor(Math.random() * 100) + 100;
-  // const decrementNumber = n => n - Math.floor(Math.random() * 10);
-  //
-  // const mainHeading = document.getElementsByClassName("main-heading")[0];
-  //
-  // const changeColors = (r, g, b) => {
-  //   r = r ? r : 0;
-  //   g = g ? g : 0;
-  //   b = b ? b : 0;
-  //   setTimeout(function() {
-  //     r = incrementNumber(r);
-  //     g = incrementNumber(g);
-  //     b = incrementNumber(b);
-  //     console.log(r, g, b);
-  //
-  //     mainHeading.setAttribute("style", `color: rgba(${r},${g},${b});`);
-  //     changeColors(r, g, b);
-  //   }, 200);
-  // };
-  // // changeColors();
   setTimeout(function() {
     const video = document.getElementById("video");
     const videoOverlay = document.getElementById("video-overlay");
