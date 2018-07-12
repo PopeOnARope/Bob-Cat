f8cefe13a27e104eea1d6a1097612ef18a266ba2
Cbodtorf <cbodtorf@gmail.com>
2018-07-11 21:28:56 -0400

    added a page title focus function.

--- src/js/main.js
+++ src/js/main.js
@@ -6,4 +6,17 @@ $(() => {
     const videoHeight = video.clientHeight + 80;
     videoOverlay.setAttribute("style", `height: ${videoHeight}px`);
   }, 100);
+
+  /**
+   * Change page title when out of focus
+   */
+  function check() {
+    if(document.hasFocus() == lastFocusStatus) return;
+    lastFocusStatus = !lastFocusStatus;
+    statusEl.innerText = lastFocusStatus ? 'Sexbruise?' : 'Baby Come Back!';
+  }
+  window.statusEl = document.getElementById('status');
+  window.lastFocusStatus = document.hasFocus();
+  check();
+  setInterval(check, 200);
 });
