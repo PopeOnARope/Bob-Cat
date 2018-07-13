ccfce417d925bb3acb3d6dde32326514717f7d62
john pope <johnpope@johns-MacBook-Pro.local>
2018-07-13 15:14:56 -0400

    some more stuff

--- src/js/main.js
+++ src/js/main.js
@@ -1,9 +1,35 @@
 $(() => {
-  setTimeout(function() {
+  const setAllTheSizes = () => {
     const video = document.getElementById("video");
     const videoOverlay = document.getElementById("video-overlay");
-    //add 80px to account for nav height
-    const videoHeight = $(window).height();
-    videoOverlay.setAttribute("style", `height: ${videoHeight}px`);
+
+    const videoHeight = $(video).height();
+    const windowHeight = $(window).height();
+    const overlayHeight =
+      videoHeight > windowHeight ? windowHeight : videoHeight;
+    videoOverlay.setAttribute("style", `height: ${overlayHeight}px`);
+
+    // resize youtube iframe based on screen width
+    const $yt = $(".youtube");
+    const ytWidth = $yt.width();
+    $yt.css("height", `${ytWidth * 0.5625}`);
+    console.log("resized");
+  };
+  setTimeout(function() {
+    setAllTheSizes();
   }, 100);
+
+  $(window).resize(setAllTheSizes());
+
+  $(".navlink").click(e => {
+    const el = e.target.attributes.href;
+    const selector = $(el.value);
+
+    $("html, body").animate(
+      {
+        scrollTop: $(`${el.value}-section`).offset().top - 81
+      },
+      1000
+    );
+  });
 });
